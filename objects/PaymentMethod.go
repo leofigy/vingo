1c6a1413b13a359f5e7f0e7e9cd854c522f5122c
Angel Figueroa <leofigy@outlook.com>
2019-04-06 17:15:56 -0600

    added more items skeletons

--- objects/PaymentMethod.go
+++ objects/PaymentMethod.go
@@ -1,3 +1,33 @@
 package objects
 
-type PaymentMethod struct{}
+import (
+	"time"
+)
+
+type PaymentMethod struct {
+	Object                string
+	ID                    string
+	VID                   string
+	Created               time.Time
+	Type                  string // check enum
+	CreditCard            *CreditCard
+	ECP                   *ECP
+	DirectDebit           *DirectDebit
+	PayPal                *PayPal
+	Boleto                *Boleto
+	HostedPage            *HostedPage
+	Token                 *Token
+	MAP                   *MerchantAcceptedPayment
+	CarrierBilling        *CarrierBilling
+	ApplePay              *ApplePay
+	Metadata              map[string]interface{}
+	AccountHolder         string
+	BillingAddress        *Address
+	CustomerSpecifiedType string
+	CustomerDescription   string
+	Currency              string
+	Primary               bool
+	Active                bool
+	ValidationTransaction *Transaction
+	Policy                map[string]interface{}
+}
