ce26b57a3e9b268e246fb1ee7d363eb14efb5cd1
Angel Figueroa <leofigy@gmail.com>
2019-04-08 22:52:30 -0500

    Update address fields

--- objects/Address.go
+++ objects/Address.go
@@ -1,4 +1,19 @@
 package objects
 
 type Address struct {
+	Object     string
+	VID        string
+	Name       string
+	Line1      string
+	Line2      string
+	Line3      string
+	City       string
+	County     string
+	District   string
+	PostalCode string
+	Country    string
+	Phone      string
+	Fax        string
+	Latitude   string
+	Longitude  string
 }
